41c399ad2c4ac3d1bc8bb9d2b671916fb299b8c3
edumatheus65 <ematheusneves13@gmail.com>
2025-03-17 16:46:00 -0300

    feat: add initial sidebar components

---
+++ app/_components/sidebar.tsx
@@ -0,0 +1,13 @@
+const Sidebar = () => {
+  return (
+    <div className="w-64 bg-white">
+      {/* Imagem */}
+      <div className="px-8 py-6">
+        <h1 className="text-2xl font-bold">GooleSheets</h1>
+      </div>
+      {/* Botões */}
+    </div>
+  );
+};
+
+export default Sidebar;
