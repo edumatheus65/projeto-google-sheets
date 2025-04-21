052e0ba1e745357e389db14534189a84b7cec82e
edumatheus65 <ematheusneves13@gmail.com>
2025-04-21 07:24:46 -0300

    fix: the column was corrected to capture the dates dashboard route.ts

--- app/(dashboard)/_components/date-picker-with-range.tsx
+++ app/(dashboard)/_components/date-picker-with-range.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { addDays, format } from "date-fns";
+import { addDays, format, parse } from "date-fns";
 import { Calendar as CalendarIcon } from "lucide-react";
 import { DateRange } from "react-day-picker";
 
@@ -13,6 +13,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/app/_components/ui/popover";
+import { Input } from "@/app/_components/ui/input";
 
 export function DatePickerWithRange({
   onDateChange,
@@ -21,9 +22,27 @@ export function DatePickerWithRange({
   onDateChange: (dates: { from: Date; to: Date }) => void;
 } & React.HTMLAttributes<HTMLDivElement>) {
   const [date, setDate] = React.useState<DateRange | undefined>({
-    from: new Date(2022, 0, 20),
-    to: addDays(new Date(2022, 0, 20), 20),
+    from: new Date(),
+    to: addDays(new Date(), 7),
   });
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    field: "from" | "to",
+  ) => {
+    const parsedDate = parse(e.target.value, "yyyy-MM-dd", new Date());
+    if (!isNaN(parsedDate.getTime())) {
+      setDate((prev) => {
+        const updated = {
+          from: field === "from" ? parsedDate : (prev?.from ?? new Date()),
+          to:
+            field === "to" ? parsedDate : (prev?.to ?? addDays(new Date(), 7)),
+        };
+
+        return updated;
+      });
+    }
+  };
 
   React.useEffect(() => {
     if (date?.from && date?.to) {
@@ -33,42 +52,41 @@ export function DatePickerWithRange({
 
   return (
     <div className={cn("grid gap-2", className)}>
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button
-            id="date"
-            variant={"outline"}
-            className={cn(
-              "w-[300px] justify-start text-left font-normal",
-              !date && "text-muted-foreground",
-            )}
-          >
-            <CalendarIcon />
-            {date?.from ? (
-              date.to ? (
-                <>
-                  {format(date.from, "LLL dd, y")} -{" "}
-                  {format(date.to, "LLL dd, y")}
-                </>
-              ) : (
-                format(date.from, "LLL dd, y")
-              )
-            ) : (
-              <span>Pick a date</span>
-            )}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-auto p-0" align="start">
-          <Calendar
-            initialFocus
-            mode="range"
-            defaultMonth={date?.from}
-            selected={date}
-            onSelect={setDate}
-            numberOfMonths={2}
-          />
-        </PopoverContent>
-      </Popover>
+      <div className="flex items-center gap-2">
+        <Input
+          type="date"
+          value={date?.from ? format(date.from, "yyyy-MM-dd") : ""}
+          onChange={(e) => handleInputChange(e, "from")}
+        />
+        <span>até</span>
+        <Input
+          type="date"
+          value={date?.to ? format(date.to, "yyyy-MM-dd") : ""}
+          onChange={(e) => handleInputChange(e, "to")}
+        />
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              id="calendar"
+              variant="outline"
+              className="w-[42px] p-2"
+              title="Abrir calendário"
+            >
+              <CalendarIcon size={20} />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0" align="start">
+            <Calendar
+              initialFocus
+              mode="range"
+              defaultMonth={date?.from}
+              selected={date}
+              onSelect={setDate}
+              numberOfMonths={2}
+            />
+          </PopoverContent>
+        </Popover>
+      </div>
     </div>
   );
 }
--- app/api/dashboard/route.ts
+++ app/api/dashboard/route.ts
@@ -51,9 +51,10 @@ export const getTaskIdByUser = async (
   const result = await prisma.$queryRaw<{ count: bigint }[]>`
       SELECT COUNT(DISTINCT task_id) AS count
       FROM activity_data
-      WHERE user_id = ${userId} AND task_id IS NOT NULL
-      AND createdAt >= ${startDate}
-      AND createdAt < ${endDate}
+      WHERE user_id = ${userId} 
+      AND task_id IS NOT NULL
+      AND custcol_20 >= ${startDate}
+      AND custcol_20 <= ${endDate}
     `;
 
   return Number(result[0]?.count ?? 0);
@@ -68,8 +69,8 @@ export const getMostWorkedLocation = async (
     SELECT locn_brcd, COUNT(*) as count
     FROM activity_data
     WHERE user_id = ${userId}
-    AND createdAt >= ${startDate}
-    AND createdAt < ${endDate}
+    AND custcol_20 >= ${startDate}
+    AND custcol_20 <= ${endDate}
     GROUP BY locn_brcd
     ORDER BY count DESC
     LIMIT 1
