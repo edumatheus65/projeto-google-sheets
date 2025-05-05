a30dcb4debbbbd11f5719be7e71482b54391e80e
edumatheus65 <ematheusneves13@gmail.com>
2025-05-05 08:46:08 -0300

    feat: add initial auth service (domain interface)

--- app/src/domains/user/services/AuthService.ts
+++ app/src/domains/user/services/AuthService.ts
@@ -1,5 +1,11 @@
+import { User } from "../entities/User";
+
 export interface AuthService {
   loginWithGoogle(): Promise<void>;
 
   logout(): Promise<void>;
+
+  getCurrentUser(): Promise<User | null>;
+
+  isAuthenticated(): Promise<boolean>;
 }
